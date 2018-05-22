06d8f07c439b817389ce635226f72823735e2101
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-22 10:10:42 -0700

    Click and press working

--- noise.js
+++ noise.js
@@ -1,3 +1,21 @@
+const playNote = function(note) {
+  $(note)[0].play();
+  $(note)[0].currentTime = 0;
+}
+
 $(document).ready( function() {
-  // your code here
+
+  $('button').click(function() {
+    let note = `#${this.innerText}Audio`
+    playNote(note);
+  });
+
+  $(this).keydown(function(event) {
+    if (event.keyCode >= 65 && event.keyCode <= 71) {
+      let note = `#${event.key}Audio`;
+      console.log(note);
+      playNote(note);
+    }
+  });
+
 });
